5ddbb5eaed7b1c06dc6e16c0d39e030691c67b19
Muhamad Faishal Khalfani <muhamadfaishal094@gmail.com>
2026-06-01 23:48:58 +0700

    feat: Menambahkan eksekusi Terraform ke CI/CD & Menambahkan monitoring

--- infra/main.tf
+++ infra/main.tf
@@ -24,3 +24,78 @@ resource "docker_container" "portfolio_web" {
     external = 8080
   }
 }
+
+# --- MONITORING & OBSERVABILITY ---
+
+# Mengunduh Image Monitoring
+resource "docker_image" "cadvisor" {
+  name         = "gcr.io/cadvisor/cadvisor:v0.47.0"
+  keep_locally = false
+}
+resource "docker_image" "prometheus" {
+  name         = "prom/prometheus:latest"
+  keep_locally = false
+}
+resource "docker_image" "grafana" {
+  name         = "grafana/grafana:latest"
+  keep_locally = false
+}
+
+# Container 1: cAdvisor (Port 8081)
+resource "docker_container" "cadvisor" {
+  image = docker_image.cadvisor.image_id
+  name  = "portfolio-cadvisor"
+  privileged = true
+  ports {
+    internal = 8080
+    external = 8081
+  }
+  
+  # Volume khusus agar cAdvisor bisa membaca mesin Docker (Diubah ke format multi-line)
+  volumes {
+    host_path      = "/"
+    container_path = "/rootfs"
+    read_only      = true
+  }
+  volumes {
+    host_path      = "/var/run"
+    container_path = "/var/run"
+    read_only      = true
+  }
+  volumes {
+    host_path      = "/sys"
+    container_path = "/sys"
+    read_only      = true
+  }
+  volumes {
+    host_path      = "/var/lib/docker/"
+    container_path = "/var/lib/docker/"
+    read_only      = true
+  }
+}
+
+# Container 2: Prometheus (Port 9090)
+resource "docker_container" "prometheus" {
+  image = docker_image.prometheus.image_id
+  name  = "portfolio-prometheus"
+  ports {
+    internal = 9090
+    external = 9090
+  }
+  volumes {
+    # Menyuntikkan file konfigurasi yang kita buat di Langkah 1
+    host_path      = abspath("../monitoring/prometheus.yml")
+    container_path = "/etc/prometheus/prometheus.yml"
+    read_only      = true
+  }
+}
+
+# Container 3: Grafana (Port 3000)
+resource "docker_container" "grafana" {
+  image = docker_image.grafana.image_id
+  name  = "portfolio-grafana"
+  ports {
+    internal = 3000
+    external = 3000
+  }
+}
